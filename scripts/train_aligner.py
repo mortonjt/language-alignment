Adding double precision to fix numerical issues

scripts/train_aligner.py
@@ -20,7 +20,7 @@ from torch.nn.utils import clip_grad_norm_
 from language_alignment import pretrained_language_models
 from language_alignment.models import AlignmentModel
 from language_alignment.layers import MeanAligner, SSAaligner, CCAaligner
-from language_alignment.losses import RankingLoss
+from language_alignment.losses import TripletLoss
 from language_alignment.dataset import AlignmentDataset
 from language_alignment.dataset import collate_alignment_pairs
 
@@ -77,13 +77,13 @@ def make_train_step(model, triplet_loss, optimizer):
     def train_step(x, y, z):
         model.train()
         optimizer.zero_grad()
-        if torch.isnan(x).sum().item() > 0:
-            print(x)
         xy = model(x, y)
+
         xz = model(x, z)
         loss = triplet_loss(xy, xz)
         loss.backward()
-        #
+        grad_params = list(filter(lambda p: p.requires_grad, model.parameters()))
+
         optimizer.step()
         # from torchviz import make_dot, make_dot_from_trace
         # dot = make_dot(loss, params=dict(model.named_parameters()))
@@ -98,7 +98,6 @@ def make_valid_step(model, triplet_loss, scheduler):
     # Builds function that performs a step in the valid loop
     def valid_step(x, y, z):
         model.eval()
-
         xy = model(x, y)
         xz = model(x, z)
         loss = triplet_loss(xy, xz)
@@ -114,6 +113,8 @@ def checkpoint_step(args, model, avg_valid_loss, best_valid_loss):
         best_valid_loss = avg_valid_loss
 
 def main(args):
+    # set seed for debugging
+    # torch.manual_seed(0)
     # Initialize model
     model, device = init_model(args)
     # Initialize tensorboard
@@ -125,8 +126,9 @@ def main(args):
     if not args.finetune:
         for p in model.lm.parameters():
             p.requires_grad = False
-        optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()),
-                               lr=args.learning_rate, weight_decay=1)
+        grad_params = list(filter(lambda p: p.requires_grad, model.parameters()))
+        optimizer = torch.optim.RMSprop(
+            grad_params, lr=args.learning_rate, weight_decay=args.reg_par)
         scheduler = ExponentialLR(optimizer, gamma=0.9)
     # else:
     #     optimizer = optim.SGD(
@@ -137,7 +139,7 @@ def main(args):
     #     )
     #     scheduler = CyclicLR(optimizer, base_lr=1e-5, max_lr=args.learning_rate)
     # Define loss function
-    triplet_loss = RankingLoss()
+    triplet_loss = TripletLoss()
     # Creates the train_step function
     train_step = make_train_step(model, triplet_loss, optimizer)
     # Creates the valid_step function
@@ -150,11 +152,9 @@ def main(args):
             if loss != loss:
                 raise ValueError("Loss is nan")
             train_loss += loss
-            #if batch_idx % 100 == 0:
-            print(loss)
-            #print("Batch {}/{}.  Batch loss: {}.".format(
-            #    batch_idx, len(train_dataloader), train_loss / (batch_idx+1)))
-
+            if batch_idx % 100 == 0:
+                print("Batch {}/{}.  Batch loss: {}.".format(
+                    batch_idx, len(train_dataloader), train_loss / (batch_idx+1)))
 
         for batch_idx, batch in enumerate(valid_dataloader):
             valid_loss += valid_step(*batch)
@@ -188,6 +188,8 @@ if __name__ == '__main__':
                         required=False, type=str)
     parser.add_argument('--learning-rate', help='Learning rate',
                         required=False, type=float, default=1e-3)
+    parser.add_argument('--reg-par', help='Regularization.',
+                        required=False, type=float, default=1e-5)
     parser.add_argument('--batch-size', help='Training batch size (needs to be 1 for cca)',
                         required=False, type=int, default=32)
     parser.add_argument('--epochs', help='Training batch size',

src/language_alignment/layers.py
@@ -52,12 +52,13 @@ class CCAaligner(nn.Module):
     def __init__(self, input_dim=512, embed_dim=64, max_len=1024, device='cpu'):
         super(CCAaligner, self).__init__()
         self.projection = nn.Linear(input_dim, embed_dim)
-        self.batch_norm = nn.BatchNorm1d(embed_dim, embed_dim)
+        #self.batch_norm = nn.BatchNorm1d(embed_dim, embed_dim)
         self.loss = CCAloss(embed_dim, device=device)
         self.input_dim = input_dim
         self.embed_dim = embed_dim
         self.max_len = max_len
         torch.nn.init.xavier_uniform(self.projection.weight)
+        self.projection.bias.data.fill_(0.1)
 
     def __call__(self, z_x, z_y):
         """
@@ -69,19 +70,13 @@ class CCAaligner(nn.Module):
 
         The input *must* be padded. Only accepts 1 pair at a time.
         """
-        z_x, z_y = torch.squeeze(z_x).t(), torch.squeeze(z_y).t()
-        x = self.projection(z_x).t()
-        y = self.projection(z_y).t()
-        # print('weight')
-        # print(self.projection.weight)
-        # print('gradient')
-        # print(self.projection.weight.grad)
 
-        # x = F.gelu(x)
-        # y = F.gelu(y)
+        # transpose for projection
+        z_x = z_x.permute(0, 2, 1).contiguous()
+        z_y = z_y.permute(0, 2, 1).contiguous()
+        x = self.projection(z_x)
+        y = self.projection(z_y)
+        x = x.permute(0, 2, 1).contiguous().double()
+        y = y.permute(0, 2, 1).contiguous().double()
 
-        # clip_grad_norm_(self.projection.weight, 3)
-        # clip_grad_norm_(self.projection.bias, 3)
-        #x = self.batch_norm(x).t()
-        #y = self.batch_norm(y).t()
         return self.loss(x, y)

src/language_alignment/losses.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-import torch.functional as F
+import torch.nn.functional as F
 
 
 class CCAloss(object):
@@ -27,9 +27,8 @@ class CCAloss(object):
         r2 = 1e-4
         eps = 1e-9
 
-        assert torch.isnan(H1).sum().item() == 0
-        assert torch.isnan(H2).sum().item() == 0
-
+        #assert torch.isnan(H1).sum().item() == 0
+        #assert torch.isnan(H2).sum().item() == 0
         H1, H2 = torch.squeeze(H1).t(), torch.squeeze(H2).t()
 
         o1 = o2 = H1.size(0)
@@ -42,25 +41,25 @@ class CCAloss(object):
 
         H1bar = H1 - H1.mean(dim=1).unsqueeze(dim=1)
         H2bar = H2 - H2.mean(dim=1).unsqueeze(dim=1)
-        assert torch.isnan(H1bar).sum().item() == 0
-        assert torch.isnan(H2bar).sum().item() == 0
+        # assert torch.isnan(H1bar).sum().item() == 0
+        # assert torch.isnan(H2bar).sum().item() == 0
 
         SigmaHat12 = (1.0 / (m - 1)) * torch.matmul(H1bar, H2bar.t())
         SigmaHat11 = (1.0 / (m - 1)) * torch.matmul(
             H1bar, H1bar.t()) + r1 * torch.eye(o1, device=self.device)
         SigmaHat22 = (1.0 / (m - 1)) * torch.matmul(
             H2bar, H2bar.t()) + r2 * torch.eye(o2, device=self.device)
-        assert torch.isnan(SigmaHat11).sum().item() == 0
-        assert torch.isnan(SigmaHat12).sum().item() == 0
-        assert torch.isnan(SigmaHat22).sum().item() == 0
+        # assert torch.isnan(SigmaHat11).sum().item() == 0
+        # assert torch.isnan(SigmaHat12).sum().item() == 0
+        # assert torch.isnan(SigmaHat22).sum().item() == 0
 
         # Calculating the root inverse of covariance matrices by using eigen decomposition
         [D1, V1] = torch.symeig(SigmaHat11, eigenvectors=True)
         [D2, V2] = torch.symeig(SigmaHat22, eigenvectors=True)
-        assert torch.isnan(D1).sum().item() == 0
-        assert torch.isnan(D2).sum().item() == 0
-        assert torch.isnan(V1).sum().item() == 0
-        assert torch.isnan(V2).sum().item() == 0
+        # assert torch.isnan(D1).sum().item() == 0
+        # assert torch.isnan(D2).sum().item() == 0
+        # assert torch.isnan(V1).sum().item() == 0
+        # assert torch.isnan(V2).sum().item() == 0
 
         # Added to increase stability
         posInd1 = torch.gt(D1, eps).nonzero()[:, 0]
@@ -86,7 +85,7 @@ class CCAloss(object):
             tmp = torch.trace(torch.matmul(Tval.t(), Tval))
             # print(tmp)
             corr = torch.sqrt(tmp)
-            assert torch.isnan(corr).item() == 0
+            # assert torch.isnan(corr).item() == 0
         else:
             # just the top self.outdim_size singular values are used
             trace_TT = torch.matmul(Tval.t(), Tval)
@@ -116,7 +115,7 @@ class RankingLoss(nn.Module):
     """
     def __call__(self, xy, xz):
         diff = xy - xz
-        score = F.logsigmoid(diff)
+        losses = F.logsigmoid(diff)
         #losses = sum(score)
         #losses = diff ** 2
         return -1 * losses